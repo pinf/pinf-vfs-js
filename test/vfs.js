fae0e26003f8b84b2bb4b27f81ac01f12b666fe3
Christoph Dorn <christoph@christophdorn.com>
2013-10-24 11:12:35 -0700

    initial commit

---
+++ test/vfs.js
@@ -0,0 +1,15 @@
+
+const PATH = require("path");
+const FS = require("fs-extra");
+const ASSERT = require("assert");
+const VFS = require("..");
+
+//const MODE = "test";
+const MODE = "write";
+
+const DEBUG = false;
+
+describe('vfs', function() {
+
+
+});
